dfe84f73e9aae307ed00e2e1057a19bdd5b797e4
krishna <156834582+The-Krish@users.noreply.github.com>
2025-12-11 11:14:25 +0530

    Fix: Add dotenv config for environment variables

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ import cors from "cors";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
 import fs from "fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
